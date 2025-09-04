f74b904fe003bb2f5e40693ccd9a2892bcda6659
Basant <basantsalama05@gmail.com>
2025-09-04 20:07:02 +0300

    add .gitignore for Xcode and Swift

---
+++ TrianglZiOSTask/TrianglZiOSTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrianglZiOSTaskApp.swift
+//  TrianglZiOSTask
+//
+//  Created by mac on 04/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrianglZiOSTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
