8a0f700f8be0c556acb198820dd1bac35244484e
Basant <basantsalama05@gmail.com>
2025-09-08 05:47:48 +0300

    Connect ContentView to PostsViewModel using LoadPostsUseCase

--- TrianglZiOSTask/Presentation/Views/ContentView.swift
+++ TrianglZiOSTask/Presentation/Views/ContentView.swift
@@ -6,21 +6,56 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @StateObject private var viewModel: PostsViewModel
+
+    init(loadPostsUseCase: LoadPostsUseCaseProtocol) {
+        _viewModel = StateObject(wrappedValue: PostsViewModel(loadPostsUseCase: loadPostsUseCase))
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            PostsListView(viewModel: viewModel)
+                .onAppear { viewModel.loadPosts() }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        let mockPosts = [
+            Post(id: 1, title: "Mock Post 1", body: "Body of mock post 1"),
+            Post(id: 2, title: "Mock Post 2", body: "Body of mock post 2")
+        ]
+        
+        // 2. Mock Use Case
+        class MockLoadPostsUseCase: LoadPostsUseCaseProtocol {
+            let posts: [Post]
+            init(posts: [Post]) { self.posts = posts }
+            
+            func execute(forceRefresh: Bool = false) -> AnyPublisher<[Post], Error> {
+                Just(posts)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+            }
+        }
+        
+        let mockUseCase = MockLoadPostsUseCase(posts: mockPosts)
+        
+    
+        return ContentView(loadPostsUseCase: mockUseCase)
     }
 }
+
+
+
+
+
+
+
+
+
--- TrianglZiOSTask/Presentation/Views/TrianglZiOSTaskApp.swift
+++ TrianglZiOSTask/Presentation/Views/TrianglZiOSTaskApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct TrianglZiOSTaskApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
+        let local = LocalPostsCache(context: persistenceController.container.viewContext)
+        let remote = RemotePostsAPI()
+        let repository = PostsRepositoryImpl(remote: remote, local: local)
+        let useCase = LoadPostsUseCase(repository: repository)
+
         WindowGroup {
-            ContentView()
+            ContentView(loadPostsUseCase: useCase)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
